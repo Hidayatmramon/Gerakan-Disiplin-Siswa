928178bb7903a953e5c43ea92f0c180ef0b04cda
Caturputraramadani <caturputraramadani@smkwikrama.sch.id>
2023-09-18 21:33:20 +0700

    update all catur's task - Back End

--- gdsApplication/registrasi.php
+++ gdsApplication/registrasi.php
@@ -1,22 +1,14 @@
 <?php
-
 require 'controler.php';
 
 if (isset($_POST["register"])) {
-
-    if (registrasi($_POST) > 0) {
-        echo "<script> 
-        alert('User baru berhasil ditambahkan!'); 
-        </script>";
+    if (registrasi($_POST)) {
+        echo "<script>alert('User baru berhasil ditambahkan!');</script>";
     } else {
-        echo mysqli_error($conn);
+        echo "<script>alert('Registrasi gagal!');</script>";
     }
 }
-
-
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -25,13 +17,11 @@ if (isset($_POST["register"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>registrasi</title>
-    <script src="https://unpkg.com/feather-icons"></script>
+    <title>Registrasi</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap" rel="stylesheet" />
     <style>
+        /* Tambahkan CSS sesuai kebutuhan Anda */
+
         .hero {
             display: flex;
             align-items: center;
@@ -42,7 +32,6 @@ if (isset($_POST["register"])) {
             background-position: center;
             position: relative;
             height: 100vh;
-
         }
 
         .hero .content {
@@ -64,22 +53,15 @@ if (isset($_POST["register"])) {
             line-height: 1.4;
             font-weight: 400;
             text-shadow: 10px 10px 30px rgba(1, 1, 3, 0.5);
-            /* mix-blend-mode: difference; */
         }
-
-
 
         .aa {
             color: #fff;
             text-decoration: none;
         }
 
-
-
         .card {
-            height: 30rem;
-
-
+            height: 35rem;
         }
 
         footer {
@@ -101,9 +83,6 @@ if (isset($_POST["register"])) {
 </head>
 
 <body>
-
-
-
     <section class="hero">
         <div class="content">
             <h1>Daftar Siswa Pengembangan Perangkat Lunak dan Gim X<span></span></h1>
@@ -113,40 +92,54 @@ if (isset($_POST["register"])) {
         </div>
         <div class="halaman">
             <div class="container">
-
-                <div class="card" style="width: 35rem; height:35rem;">
+                <div class="card" style="width: 35rem;">
                     <div class="card-body">
                         <h1 class="card-title" style="text-align: center;">Registrasi</h1>
-
                         <form action="" method="post">
-
                             <div class="mb-3">
-                                <label for="username" class="form-label">username :</label>
-                                <input type="text" class="form-control" name="username" id="username">
+                                <label for="username" class="form-label">Username:</label>
+                                <input type="text" class="form-control" name="username" id="username" required>
                             </div>
                             <div class="mb-3">
-                                <label for="password" class="form-label">password :</label>
-                                <input type="password" class="form-control" name="password" id="password">
+                                <label for="nama" class="form-label">nama :</label>
+                                <input type="text" class="form-control" name="nama" id="nama">
                             </div>
                             <div class="mb-3">
-                                <label for="password2" class="form-label">konfirmasi password :</label>
-                                <input type="password" class="form-control" name="password2" id="password2">
+                                <label for="password" class="form-label">Password:</label>
+                                <input type="password" class="form-control" name="password" id="password" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="password2" class="form-label">Konfirmasi Password:</label>
+                                <input type="password" class="form-control" name="password2" id="password2" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="rayon" class="form-label">Rayon</label>
+                                <select class="form-select" name="rayon" id="rayon" required>
+                                    <option value="Cisarua 1">Cisarua 1</option>
+                                    <option value="Cisarua 2">Cisarua 2</option>
+                                    <option value="Cisarua 3">Cisarua 3</option>
+                                    <option value="Cisarua 4">Cisarua 4</option>
+                                    <option value="Cisarua 5">Cisarua 5</option>
+                                    <option value="Cisarua 6">Cisarua 6</option>
+                                    <option value="Cisarua 7">Cisarua 7</option>
+                                    <option value="Wikrama 1">Wikrama 1</option>
+                                    <option value="Wikrama 2">Wikrama 2</option>
+                                    <option value="Wikrama 3">Wikrama 3</option>
+                                    <option value="Wikrama 4">Wikrama 4</option>
+                                    <option value="Wikrama 5">Wikrama 5</option>
+                                </select>
                             </div>
                             <div class="d-grid gap-2">
-                                <button type="submit" class="text-white btn bg-secondary bg-gradient" name="register"> Register !</button>
+                                <button type="submit" class="text-white btn bg-secondary bg-gradient" name="register">Register</button>
                             </div>
                             <br>
                             <button type="button" class="btn bg-secondary bg-gradient"><a class="aa" href="Login.php">Login</a></button>
                         </form>
-
                     </div>
                 </div>
-
             </div>
         </div>
     </section>
-
-
 </body>
 
 </html>
